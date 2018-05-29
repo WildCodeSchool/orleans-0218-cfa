1cf0f018a7d99688b214ad7859fe13897a7de392
Nassgr <anas.grina@hotmail.fr>
2018-05-29 16:49:40 +0200

    création du slider avec images fixe donné par le client

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,5 @@
 import "../../node_modules/bootstrap/scss/bootstrap.scss";
+import "../../node_modules/bootstrap/dist/js/bootstrap.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery/dist/jquery.min.js";
+import "jquery/dist/jquery.slim"
