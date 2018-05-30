0de4c0030d16dab120a201c645504f4154ab0ac5
Nassgr <anas.grina@hotmail.fr>
2018-05-30 17:43:22 +0200

    first step

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 import "../../node_modules/bootstrap/scss/bootstrap.scss";
 import "../../node_modules/bootstrap/dist/js/bootstrap.js";
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import "jquery/dist/jquery.min.js";
 import "jquery/dist/jquery.slim"
