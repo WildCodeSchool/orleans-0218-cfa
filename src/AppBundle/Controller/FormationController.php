997fa3a8d2924b684e00bdf17b126a2873a7429e
dju45 <hostejulien@gmail.com>
2018-06-06 18:22:29 +0200

    add use to formation controller

--- src/AppBundle/Controller/FormationController.php
+++ src/AppBundle/Controller/FormationController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Formation;
 
 /**
  * Formation controller.
