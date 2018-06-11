dab74228c10d104293a03f41448d00d87b1ff728
Nassgr <35959360+Nassgr@users.noreply.github.com>
2018-06-11 16:23:20 +0200

    Update FormationController.php

--- src/AppBundle/Controller/FormationController.php
+++ src/AppBundle/Controller/FormationController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request; 
 use AppBundle\Entity\Formation;
 
 /**
