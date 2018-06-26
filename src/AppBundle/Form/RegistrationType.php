e63170fa934323d520be29cd4cdc5307ee270814
Thomas Essoungou <essoungou_thomas@hotmail.fr>
2018-06-26 14:41:31 +0200

    fix travis

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -8,34 +8,30 @@
 
 namespace AppBundle\Form;
 
-
 use Symfony\Component\Form\AbstractType ;
 use Symfony\Component\Form\FormBuilderInterface ;
 
 class RegistrationType extends AbstractType
 {
-    public function buildForm ( FormBuilderInterface $builder , array $options )
+    public function buildForm(FormBuilderInterface $builder , array $options)
     {
-        $builder -> add ( 'name' );
-
+        $builder
+            ->add('name');
     }
 
-    public function getParent ()
+    public function getParent()
     {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType' ;
 
-        // Or for Symfony < 2.8
-        // return 'fos_user_registration';
     }
 
-    public function getBlockPrefix ()
+    public function getBlockPrefix()
     {
         return 'app_user_registration' ;
     }
 
-    // For Symfony 2.x
-    public function getName ()
+    public function getName()
     {
-        return $this -> getBlockPrefix ();
+        return $this->getBlockPrefix();
     }
 }
