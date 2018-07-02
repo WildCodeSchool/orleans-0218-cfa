f5dce873f421ea64d5ef89e815d9bf73205e7c19
dju45 <hostejulien@gmail.com>
2018-07-02 22:29:43 +0200

    add upload  fields to partner entity

--- src/AppBundle/Entity/Partnair.php
+++ src/AppBundle/Entity/Partnair.php
@@ -4,12 +4,14 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Partnair
  *
  * @ORM\Table(name="partnair")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\PartnairRepository")
+ * @Vich\Uploadable
  */
 class Partnair
 {
@@ -22,12 +24,7 @@ class Partnair
      */
     private $id;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="image", type="string", length=150, nullable=true)
-     */
-    private $image;
+
 
     /**
      * @var string
@@ -55,6 +52,38 @@ class Partnair
      * @ORM\Column(name="name", type="string", length=80)
      */
     private $name;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * @var string|null
+     * @Vich\UploadableField(mapping="formation_ref", fileNameProperty="imageName")
+
+     * @ORM\Column(name="image", type="string", length=150, nullable=true)
+     */
+    private $image;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     *
+     * @Vich\UploadableField(mapping="formation_ref", fileNameProperty="referentielName")
+     * @ORM\Column(nullable=true)
+     * @var File
+     */
+    private $referentielFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @var string
+     */
+    private $imageName;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
 
 
     /**
@@ -138,4 +167,64 @@ class Partnair
     {
         return $this->name;
     }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $referentiel
+     */
+    public function setReferentielFile(?File $referentiel = null): void
+    {
+        $this->referentielFile = $referentiel;
+
+        if (null !== $referentiel) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getReferentielFile(): ?File
+    {
+        return $this->referentielFile;
+    }
+
+    public function setReferentielName(?string $referentielName): void
+    {
+        $this->referentielName = $referentielName;
+    }
+
+    public function getReferentielName(): ?string
+    {
+        return $this->referentielName;
+    }
+
+    /**
+     * Set updatedAt.
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Formation
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt.
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
 }
