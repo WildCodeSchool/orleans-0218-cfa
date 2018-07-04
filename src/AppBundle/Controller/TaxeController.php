a1124f2dbc5993d05c729d0e917b97821bc36f92
Nassgr <anas.grina@hotmail.fr>
2018-07-04 16:00:07 +0200

    ajout des routes

--- src/AppBundle/Controller/TaxeController.php
+++ src/AppBundle/Controller/TaxeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * Taxe controller.
  *
- * @Route("cfabloisadmin/taxe")
+ * @Route("/taxe")
  */
 class TaxeController extends Controller
 {
@@ -30,108 +30,5 @@ class TaxeController extends Controller
         return $this->render('taxe/index.html.twig', array(
             'taxes' => $taxes,
         ));
-    }
-
-    /**
-     * Creates a new taxe entity.
-     *
-     * @Route("/new", name="taxe_new")
-     * @Method({"GET", "POST"})
-     */
-    public function newAction(Request $request)
-    {
-        $taxe = new Taxe();
-        $form = $this->createForm('AppBundle\Form\TaxeType', $taxe);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($taxe);
-            $em->flush();
-
-            return $this->redirectToRoute('taxe_show', array('id' => $taxe->getId()));
-        }
-
-        return $this->render('taxe/new.html.twig', array(
-            'taxe' => $taxe,
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
-     * Finds and displays a taxe entity.
-     *
-     * @Route("/{id}", name="taxe_show")
-     * @Method("GET")
-     */
-    public function showAction(Taxe $taxe)
-    {
-        $deleteForm = $this->createDeleteForm($taxe);
-
-        return $this->render('taxe/show.html.twig', array(
-            'taxe' => $taxe,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing taxe entity.
-     *
-     * @Route("/{id}/edit", name="taxe_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Taxe $taxe)
-    {
-        $deleteForm = $this->createDeleteForm($taxe);
-        $editForm = $this->createForm('AppBundle\Form\TaxeType', $taxe);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('taxe_edit', array('id' => $taxe->getId()));
-        }
-
-        return $this->render('taxe/edit.html.twig', array(
-            'taxe' => $taxe,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a taxe entity.
-     *
-     * @Route("/{id}", name="taxe_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Taxe $taxe)
-    {
-        $form = $this->createDeleteForm($taxe);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($taxe);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('taxe_index');
-    }
-
-    /**
-     * Creates a form to delete a taxe entity.
-     *
-     * @param Taxe $taxe The taxe entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Taxe $taxe)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('taxe_delete', array('id' => $taxe->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
     }
 }
