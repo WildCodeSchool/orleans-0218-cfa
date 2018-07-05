e5882e78e910df74dab3e31a44a98e6f0534e437
dju45 <hostejulien@gmail.com>
2018-07-05 23:48:38 +0200

    generate crud

---
+++ src/AppBundle/Controller/FaqController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Faq;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Faq controller.
+ *
+ * @Route("faq")
+ */
+class FaqController extends Controller
+{
+    /**
+     * Lists all faq entities.
+     *
+     * @Route("/", name="faq_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $faqs = $em->getRepository('AppBundle:Faq')->findAll();
+
+        return $this->render('faq/index.html.twig', array(
+            'faqs' => $faqs,
+        ));
+    }
+
+    /**
+     * Creates a new faq entity.
+     *
+     * @Route("/new", name="faq_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $faq = new Faq();
+        $form = $this->createForm('AppBundle\Form\FaqType', $faq);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($faq);
+            $em->flush();
+
+            return $this->redirectToRoute('faq_show', array('id' => $faq->getId()));
+        }
+
+        return $this->render('faq/new.html.twig', array(
+            'faq' => $faq,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a faq entity.
+     *
+     * @Route("/{id}", name="faq_show")
+     * @Method("GET")
+     */
+    public function showAction(Faq $faq)
+    {
+        $deleteForm = $this->createDeleteForm($faq);
+
+        return $this->render('faq/show.html.twig', array(
+            'faq' => $faq,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing faq entity.
+     *
+     * @Route("/{id}/edit", name="faq_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Faq $faq)
+    {
+        $deleteForm = $this->createDeleteForm($faq);
+        $editForm = $this->createForm('AppBundle\Form\FaqType', $faq);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('faq_edit', array('id' => $faq->getId()));
+        }
+
+        return $this->render('faq/edit.html.twig', array(
+            'faq' => $faq,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a faq entity.
+     *
+     * @Route("/{id}", name="faq_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Faq $faq)
+    {
+        $form = $this->createDeleteForm($faq);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($faq);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('faq_index');
+    }
+
+    /**
+     * Creates a form to delete a faq entity.
+     *
+     * @param Faq $faq The faq entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Faq $faq)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('faq_delete', array('id' => $faq->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Form/FaqType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FaqType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('question')->add('response');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Faq'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_faq';
+    }
+
+
+}
